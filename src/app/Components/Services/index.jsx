b86434fc3e14bd859e53ca1be502707affd68bd4
morshidulrahman <boydanger416@gmail.com>
2023-02-03 00:05:44 -0500

    services_done

--- src/app/Components/Services/index.jsx
+++ src/app/Components/Services/index.jsx
@@ -1,17 +1,36 @@
+import { motion } from "framer-motion";
 import React from "react";
 import serviceData from "../../assets/data/serviceData";
+import { Color } from "../configs/Color";
+
 function Services() {
   return (
-    <div className="container mx-auto px-6">
-      <div>
+    <section className="container mx-auto px-6 py-8">
+      <div className="flex items-center justify-between">
         {serviceData.map((item, index) => (
-          <div className="flex gap-2" key={index}>
-            <p>{item.title}</p>
-            <p>{item.subtitle}</p>
-          </div>
+          <motion.div
+            whileHover={{ sclae: 1.2 }}
+            className="flex space-x-2 my-2 px-1 py-2 rounded-sm items-center w-[22%] hover:-top-2 duration-300"
+            style={{ background: `${item.bg}` }}
+            key={index}
+          >
+            <span
+              className={`w-8 h-8 bg-[${Color.primarycolor}] text-white flex items-center justify-center rounded-full`}
+            >
+              {item.icon}
+            </span>
+            <div>
+              <p
+                className={`text-[${Color.primarycolor}] text-sm font-semibold py-1`}
+              >
+                {item.title}
+              </p>
+              <p className="text-xs text-[#999]">{item.subtitle}</p>
+            </div>
+          </motion.div>
         ))}
       </div>
-    </div>
+    </section>
   );
 }
 
--- src/app/assets/data/serviceData.jsx
+++ src/app/assets/data/serviceData.jsx
@@ -1,24 +1,28 @@
+import { AiOutlineDollarCircle } from "react-icons/ai";
+import { TbRefresh } from "react-icons/tb";
+import { GiPayMoney } from "react-icons/gi";
+import { MdOutlineLocalShipping } from "react-icons/md";
 const serviceData = [
   {
-    icon: "ri-truck-line",
+    icon: <MdOutlineLocalShipping size={18} />,
     title: "Free Shipping",
     subtitle: "Lorem ipsum dolor sit amet.",
     bg: "#fdefe6",
   },
   {
-    icon: "ri-refresh-line",
+    icon:  <TbRefresh size={18} />,
     title: "Easy Returns",
     subtitle: "Lorem ipsum dolor sit amet.",
     bg: "#ceebe9",
   },
   {
-    icon: "ri-secure-payment-line",
+    icon: <GiPayMoney size={18} />,
     title: "Secure Payment",
     subtitle: "Lorem ipsum dolor sit amet.",
     bg: "#e2f2b2",
   },
   {
-    icon: "ri-exchange-dollar-line",
+    icon:<AiOutlineDollarCircle  size={18}/>,
     title: " Back Guarantee",
     subtitle: "Lorem ipsum dolor sit amet.",
     bg: "#d6e5fb",
